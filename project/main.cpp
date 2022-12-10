e6ce3a7c88f5b210d6b31a9e5714242eb54f07a5
IgorRunny <ibolshacov@yandex.ru>
2022-12-11 00:46:14 +0200

    now it can push off!

--- project/main.cpp
+++ project/main.cpp
@@ -3,24 +3,37 @@
 #include <vector>
 #include <thread>
 #include <chrono>
+#include<math.h>
+double PI = acos(-1);
+
+void Angle(int& angle)
+{
+    angle += 30;
+    if (angle >= 360)
+    {
+        angle -= 360;
+    }
+}
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
     sf::CircleShape shape(20);
     shape.setFillColor(sf::Color::Red);
-    shape.setPosition(100, 600);
+    shape.setPosition(10, 600);
     sf::Clock timer;
-    shape.setOrigin(100, 100);
-    sf::Vector2i defPos = sf::Vector2i(500, 500);
+    shape.setOrigin(20, 20);
+    sf::Vector2i defPos = sf::Vector2i(425, 900);
     sf::Mouse::setPosition(defPos, window);
     sf::RectangleShape platform(sf::Vector2f(150,10));
     platform.setFillColor(sf::Color(0, 166, 147));
-    platform.setPosition(425, 900);
-    platform.setOrigin(75, 5);
-    int x = 1;
-    int y = 2;
-    
+    platform.setPosition(sf::Vector2f(defPos));
+    platform.setOrigin(platform.getSize().x / 2, platform.getSize().y / 2);
+    double speed = 0.5;
+    int angle = 30;
+    double x = 100;
+    double y = 600;
+    shape.setPosition(x, y);
     
     
     
@@ -29,7 +42,11 @@ int main()
     while (window.isOpen())
     {
         sf::Event event;
-        shape.move(x, y);
+        sf::Time dt = timer.restart();
+        std::cout << dt.asMicroseconds() << "\n";
+        x += speed  * cosf(angle * PI / 180);
+        y += speed * sinf(angle * PI / 180);
+        shape.setPosition(x, y);
         if (sf::Mouse::getPosition(window).x < 0)
         {
             sf::Mouse::setPosition(sf::Vector2i(0, sf::Mouse::getPosition(window).y), window);
@@ -43,10 +60,25 @@ int main()
             platform.setPosition(sf::Mouse::getPosition(window).x, platform.getPosition().y);
         }
         std::cout << platform.getPosition().x << "   " << shape.getPosition().x << "\n";
-        if (shape.getPosition().x > platform.getPosition().x - 75 && shape.getPosition().x < platform.getPosition().x + 75 && shape.getPosition().y == 800)
+        if (x + shape.getRadius() >= 1000)
         {
-            int newx = (shape.getPosition().x - platform.getPosition().x) + shape.getRadius() * 2;
-            x = newx - platform.getPosition().x;
+            Angle(angle);
+        }
+        if (x - shape.getRadius() <= 0)
+        {
+            Angle(angle);
+        }
+        if (y + shape.getRadius() >= 1000)
+        {
+            Angle(angle);
+        }
+        if (y - shape.getRadius() <= 0)
+        {
+            Angle(angle);
+        }
+        if ((x + shape.getRadius() >= platform.getPosition().x - platform.getSize().x / 2) && (x + shape.getRadius() <= platform.getPosition().x + platform.getSize().x / 2) && (y + shape.getRadius() >= platform.getPosition().y + platform.getSize().y / 2))
+        {
+            angle += 60;
         }
         while (window.pollEvent(event))
         {
@@ -58,7 +90,7 @@ int main()
         window.draw(platform);
         window.draw(shape);
         window.display();
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        //std::this_thread::sleep_for(std::chrono::milliseconds(1) / 10);
     }
 
     return 0;
